cd897d1f849e152def07df18e1ea54bd18961865
BruceWaynnee <hengpacific@gmail.com>
2022-05-01 12:48:43 +0700

    first commit [default]

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-hotel-booking-application/app';
+import config from 'ember-hotel-booking-application/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
