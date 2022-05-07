0c94ce04ebafdfc41e107487452904de864b6930
BruceWaynnee <hengpacific@gmail.com>
2022-05-07 16:26:36 +0700

    add zoom in and out feature on clicking image

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function (hooks) {
@@ -16,5 +16,27 @@ module('Integration | Component | rental/image', function (hooks) {
       .exists()
       .hasAttribute('src', '/assets/images/teaching-tomster.png')
       .hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('clicking on image component to toggle the zoom-in zoom-out size', async function(assert){
+    await render(hbs`<Rental::Image
+      src="/assets/images/teaching-tomster.png"
+      alt="Teaching Tomster"
+    />`);
+
+    assert.dom('button.image').exists();
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large');
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
